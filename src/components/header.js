87a59495fae5d2b0a450e9a5614f5f4a97f09ceb
MSturges <Sturges3232@gmail.com>
2017-02-16 13:03:04 -0700

    header styled and now a reusable component

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle} = styles;
   return (
     <View style={viewStyle} >
-      <Text style={textStyle} >Albums!</Text>
+      <Text style={textStyle} >{props.headerText}</Text>
     </View>
   );
 };
@@ -13,10 +13,22 @@ const Header = () => {
 const styles = {
 
   viewStyle: {
-    backgroundColor: 'rgb(214, 255, 254)'
-    height: 200
-  },
+    backgroundColor: 'rgb(214, 255, 254)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: 'rgb(0, 0, 0)',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.2,
+    position: 'relative',
+    elevation: 2
 
+
+  },
   textStyle: {
     fontSize: 30
   }
