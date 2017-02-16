efae65168b5d33c063bf3994d2a4c15454b3c6b1
MSturges <Sturges3232@gmail.com>
2017-02-16 10:59:08 -0700

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,11 @@
+// Place code in here for ios
+
+// Import a library to help create a component
+
+import React from 'react';
+import ReactNative from 'react-native';
+
+
+// create a component
+
+// Render component to the device
