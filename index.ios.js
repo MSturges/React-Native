5e553b0fe76745e1d1cd408f3b5577595d9f0c95
MSturges <Sturges3232@gmail.com>
2017-02-16 15:56:42 -0700

    done for day

--- index.ios.js
+++ index.ios.js
@@ -3,15 +3,15 @@
 // Import a library to help create a component
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
-import Header from "./src/components/header";
-import AlbumList from "./src/components/AlbumList";
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 
 // create a component
 const App = () => (
   <View>
     <Header headerText={'Albums!'} />
-    <AlbumList/>
+    <AlbumList />
   </View>
 );
 
