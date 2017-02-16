2ac2d70af27bf80f104154813af904d052994b2d
MSturges <Sturges3232@gmail.com>
2017-02-16 11:26:07 -0700

    rendered some text

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,20 @@
 // Place code in here for ios
 
 // Import a library to help create a component
-
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 
 
 // create a component
+const App = () => {
+  return (
+    <Text>Some text</Text>
+  );
+}
+
+// const App = () => (
+//     <Text>Some text</Text>
+// );
 
 // Render component to the device
+AppRegistry.registerComponent('ReactNativeAlbums', () => App);
